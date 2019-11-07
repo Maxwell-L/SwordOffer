c68cd6ed9fb847722530d3429c9792f8468a8932
Maxwell-L <974230257@qq.com>
2019-11-07 16:14:32 +0800

    Create Duplicate

---
+++ Code/DuplicateSolution.java
@@ -0,0 +1,32 @@
+/* 
+ * 数组中重复的数字
+ * 题目描述:
+ *     在一个长度为n的数组里的所有数字都在0到n-1的范围内。 数组中某些数字是重复的，但不知道有几个数字是重复的。也不知道每个数字重复几次。
+ * 请找出数组中任意一个重复的数字。 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。
+ *
+ * 思路:
+ *     遍历数组，将第一次出现的数值作为 key 存储到 HashMap 中，当重复数字出现时，HashMap 检测到该 key 不为空，说明重复，输出重复值并返回true。
+ */
+import java.util.HashMap;
+
+public class DuplicateSolution {
+    // Parameters:
+    //    numbers:     an array of integers
+    //    length:      the length of array numbers
+    //    duplication: (Output) the duplicated number in the array number,length of duplication array is 1,so using duplication[0] = ? in implementation;
+    //                  Here duplication like pointor in C/C++, duplication[0] equal *duplication in C/C++
+    //    这里要特别注意~返回任意重复的一个，赋值duplication[0]
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i = 0; i < length; i++){
+            if(hm.get(numbers[i]) != null){
+                duplication[0] = numbers[i];
+                return true;
+            }
+            hm.put(numbers[i], 1);
+        }
+        return false;
+    }
+}
